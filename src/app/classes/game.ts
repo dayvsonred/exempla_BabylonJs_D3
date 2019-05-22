b72634f59d393538bdad546956c79fd81b7e69cf
Dayvson <dayvson.red@gmail.com>
2019-05-21 22:15:49 -0300

    add events clicks

--- src/app/classes/game.ts
+++ src/app/classes/game.ts
@@ -1,6 +1,11 @@
 import {Engine, Scene, FreeCamera, Light,
-Vector3, HemisphericLight, MeshBuilder, PointLight, ArcRotateCamera, StandardMaterial, Color3, Texture
+Vector3, HemisphericLight, MeshBuilder, PointLight, ArcRotateCamera, StandardMaterial, Color3, Texture,
+ActionManager , ExecuteCodeAction
 } from 'babylonjs';
+
+// import {  Button3D, AdvancedDynamicTexture, Button  } from 'babylonjs-gui';
+// import * as GUI from 'babylonjs-gui';
+// import { AdvancedDynamicTexture } from '@babylonjs/gui/2D';
 
 
 export class Game {
@@ -10,6 +15,7 @@ export class Game {
         private _camera: ArcRotateCamera;
         private _light: Light;
         private _lightP: Light;
+       
   constructor(canvasElement : string) {
   this._canvas = <HTMLCanvasElement>document.getElementById(canvasElement);
   this._engine = new Engine(this._canvas, true);    
@@ -62,11 +68,18 @@ export class Game {
    var verde = new StandardMaterial("verdeMat", this._scene);
    verde.emissiveColor = new Color3(0,1,0);
 
+   var verde2 = new StandardMaterial("verde2Mat", this._scene);
+   verde2.emissiveColor = new Color3((57/255),(203/255),(64/255));
+
    var azul = new StandardMaterial("azulMat", this._scene);
    azul.emissiveColor = new Color3((15/255),(13/255),(243/255));
 
    var amarelo = new StandardMaterial("amareloMat", this._scene);
    amarelo.emissiveColor = new Color3((210/255),(181/255),0);
+
+
+   var preto = new StandardMaterial("pretoMat", this._scene);
+   preto.emissiveColor = new Color3((18/255),(10/255),(44/255));
 
 
    // texturas
@@ -218,9 +231,9 @@ export class Game {
     // plane.parent = cylindro12;
     // plane.position.y = 2;
 
-    // var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(plane);
+    //  var advancedTexture =  AdvancedDynamicTexture.CreateForMesh(plane,  2,  2);
 
-    // var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Click Me");
+    // var button1 =  Button.CreateSimpleButton("but1", "Click Me");
     // button1.width = 1;
     // button1.height = 0.4;
     // button1.color = "white";
@@ -230,6 +243,91 @@ export class Game {
     //     alert("you did it!");
     // });
     // advancedTexture.addControl(button1);
+
+
+
+
+    // evento click
+    // Os gatilhos disponíveis para malhas são: 
+    // BABYLON.ActionManager.NothingTrigger: Nunca levantado. Usado para sub-ações com action.thenfunção.
+    // BABYLON.ActionManager.OnPickTrigger: Gerado quando o usuário toca / clica em uma malha.
+    // BABYLON.ActionManager.OnDoublePickTrigger: Gerado quando o usuário toca duas vezes / clica em uma malha.
+    // BABYLON.ActionManager.OnPickDownTrigger: Gerado quando o usuário toca / clica em uma malha
+    // BABYLON.ActionManager.OnPickUpTrigger: Gerado quando o usuário toca / clica em uma malha.
+    // BABYLON.ActionManager.OnPickOutTrigger: Gerado quando o usuário toca / clica em uma malha e depois sai da malha.
+    // BABYLON.ActionManager.OnLeftPickTrigger: Gerado quando o usuário toca / clica em uma malha com o botão esquerdo.
+    // BABYLON.ActionManager.OnRightPickTrigger: Gerado quando o usuário toca / clica em uma malha com o botão direito.
+    // BABYLON.ActionManager.OnCenterPickTrigger: Gerado quando o usuário toca / clica em uma malha com o botão central.
+    // BABYLON.ActionManager.OnLongPressTrigger: Gerado quando o usuário toca / clica para cima em uma malha por um longo período de tempo em milissegundos (definido por BABYLON.Scene.LongPressDelay).
+    // BABYLON.ActionManager.OnPointerOverTrigger: Gerado quando o ponteiro está sobre uma malha. Criado apenas uma vez.
+    // BABYLON.ActionManager.OnPointerOutTrigger: Gerado quando o ponteiro não está mais sobre uma malha. Criado apenas uma vez.
+    // BABYLON.ActionManager.OnIntersectionEnterTrigger: Gerado quando a malha está em interseção com uma malha específica. Criado apenas uma vez.
+    // BABYLON.ActionManager.OnIntersectionExitTrigger: Gerado quando a malha não está mais na interseção com uma malha específica. Criado apenas uma vez.
+    
+    var sphere = MeshBuilder.CreateSphere("sphere1",  {segments: 16, diameter: 2}, this._scene);
+	sphere.position.x = 2;
+	sphere.actionManager = new ActionManager(this._scene);
+	sphere.actionManager.registerAction(new ExecuteCodeAction( ActionManager.OnPickUpTrigger, function () {
+		alert('sphere clicked');
+    }));
+    
+
+    
+    CyposX = CyposX + 3;
+    let cylindro20 = MeshBuilder.CreateCylinder('cylindre_22', { diameterTop: 2, diameterBottom: 2, tessellation: 20, subdivisions: 6 }, this._scene)
+    cylindro20.rotation.x = (Math.PI * 90) /180;
+    cylindro20.position.x = CyposX;
+    cylindro20.position.y = CyposY;
+    cylindro20.position.z = CyposZ;
+    cylindro20.material = vermelho;
+    cylindro20.actionManager = new ActionManager(this._scene);
+	cylindro20.actionManager.registerAction(new ExecuteCodeAction( ActionManager.OnPickUpTrigger, function () {
+		alert('call click event =)');
+    }));
+
+
+    preto
+
+    let numsCx = -20; let  numCz = 10; let numCy = 1;
+    let cylindro30 = MeshBuilder.CreateCylinder('cylindre_30', { diameterTop: 2, diameterBottom: 2, tessellation: 20, subdivisions: 6 }, this._scene)
+    cylindro30.rotation.x = (Math.PI * 90) /180;
+    cylindro30.rotation.y = (Math.PI * 90) /180;
+    cylindro30.position.x = -22;
+    cylindro30.position.y = 1;
+    cylindro30.position.z = 12;
+    cylindro30.material = verde2;
+    cylindro30.actionManager = new ActionManager(this._scene);
+	cylindro30.actionManager.registerAction(new ExecuteCodeAction( ActionManager.OnPickUpTrigger, function () {
+		alert('call click event =) verde');
+    }));
+
+
+    numsCx += -3; numCz += 2;
+    let cylindro32 = MeshBuilder.CreateCylinder('cylindre_32', { diameterTop: 2, diameterBottom: 2, tessellation: 20, subdivisions: 6 }, this._scene)
+    cylindro32.rotation.x = (Math.PI * 90) /180;
+    cylindro32.rotation.y = (Math.PI * 90) /180;
+    cylindro32.position.x = -22;
+    cylindro32.position.y = 1;
+    cylindro32.position.z = 10;
+    cylindro32.material = amarelo;
+    cylindro32.actionManager = new ActionManager(this._scene);
+	cylindro32.actionManager.registerAction(new ExecuteCodeAction( ActionManager.OnPickUpTrigger, function () {
+		alert('call click event =) amarelo');
+    }));
+
+    let cylindro33 = MeshBuilder.CreateCylinder('cylindre_33', { diameterTop: 2, diameterBottom: 2, tessellation: 20, subdivisions: 6 }, this._scene)
+    cylindro33.rotation.x = (Math.PI * 90) /180;
+    cylindro33.rotation.y = (Math.PI * 90) /180;
+    cylindro33.position.x = -22;
+    cylindro33.position.y = 2.8;
+    cylindro33.position.z = 11;
+    cylindro33.material = preto;
+    cylindro33.actionManager = new ActionManager(this._scene);
+	cylindro33.actionManager.registerAction(new ExecuteCodeAction( ActionManager.OnPickUpTrigger, function () {
+		alert('call click event =) normal');
+    }));
+    
+
    
 
     
